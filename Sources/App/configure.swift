666e8b59393d74af9ffb694f5e0342fe04ca88b2
Akshit Zaveri <akshit.zaveri@gmail.com>
2020-03-26 14:06:42 +0530

    Refactoring to function too long

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,21 +2,39 @@ import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
-public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+public func configure(
+  _ config: inout Config,
+  _ env: inout Environment,
+  _ services: inout Services
+) throws {
+  try registerProvider(for: &services)
+  try configureRouter(for: &services)
+  configureMiddleware(for: &services)
+  configureDatabase(for: &services)
+  configureMigrations(for: &services)
+}
+
+private func registerProvider(for services: inout Services) throws {
   // Register providers first
   try services.register(FluentPostgreSQLProvider())
+}
 
+private func configureRouter(for services: inout Services) throws {
   // Register routes to the router
   let router = EngineRouter.default()
   try routes(router)
   services.register(router, as: Router.self)
+}
 
+private func configureMiddleware(for services: inout Services) {
   // Register middleware
   var middlewares = MiddlewareConfig() // Create _empty_ middleware config
   // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
   middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
   services.register(middlewares)
+}
 
+private func configureDatabase(for services: inout Services) {
   // Configure a PostgreSQL database
   let postgreSQLConfig: PostgreSQLDatabaseConfig!
   if let url = Environment.get("DATABASE_URL") {
@@ -34,7 +52,9 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   var databases = DatabasesConfig()
   databases.add(database: postgreSQL, as: .psql)
   services.register(databases)
+}
 
+private func configureMigrations(for services: inout Services) {
   // Configure migrations
   var migrations = MigrationConfig()
   migrations.add(model: Stock.self, database: .psql)
